6ceac84f690a6774bfa102dbe15aa4d57aa3c513
Dustin Schroeder <dustin.p.schroeder@gmail.com>
2017-09-30 17:23:44 -0700

    added ghosts and ghost movement.  collision is finished.  game over works but kinda wonky

--- Pacman.js
+++ Pacman.js
@@ -5,13 +5,13 @@ var world =[
         [1,2,2,2,1,2,2,2,1,2,2,2,1,2,2,2,2,2,2,2,1,2,2,2,1,2,2,2,1,2,2,2,1],
         [1,2,1,2,1,2,1,2,1,2,2,2,1,2,1,1,1,1,1,2,1,2,2,2,1,2,1,2,1,2,1,2,1],
         [1,2,1,2,2,2,1,2,2,2,1,2,2,2,2,2,1,2,2,2,2,2,1,2,2,2,1,2,2,2,1,2,1],
-        [1,2,1,1,1,2,1,2,1,2,1,2,1,2,2,2,1,2,2,2,1,2,1,2,1,2,1,2,1,1,1,2,1],
-        [1,2,2,2,2,2,1,2,2,2,1,2,2,2,1,1,1,1,1,2,2,2,1,2,2,2,1,2,2,2,2,2,1],
+        [1,2,1,1,1,2,1,2,1,2,1,2,1,2,2,2,2,2,2,2,1,2,1,2,1,2,1,2,1,1,1,2,1],
+        [1,2,2,2,2,2,1,2,2,2,1,2,2,2,1,1,2,1,1,2,2,2,1,2,2,2,1,2,2,2,2,2,1],
         [1,2,1,2,1,2,2,2,1,2,2,2,1,2,1,0,0,0,1,2,1,2,2,2,1,2,2,2,1,2,1,2,1],
         [1,2,1,2,1,2,2,2,1,1,1,1,1,2,2,0,0,0,2,2,1,1,1,1,1,2,2,2,1,2,1,2,1],
         [1,2,1,2,1,2,2,2,1,2,2,2,1,2,1,0,0,0,1,2,1,2,2,2,1,2,2,2,1,2,1,2,1],
-        [1,2,2,2,2,2,1,2,2,2,1,2,2,2,1,1,1,1,1,2,2,2,1,2,2,2,1,2,2,2,2,2,1],
-        [1,2,1,1,1,2,1,2,1,2,1,2,1,2,2,2,1,2,2,2,1,2,1,2,1,2,1,2,1,1,1,2,1],
+        [1,2,2,2,2,2,1,2,2,2,1,2,2,2,1,1,2,1,1,2,2,2,1,2,2,2,1,2,2,2,2,2,1],
+        [1,2,1,1,1,2,1,2,1,2,1,2,1,2,2,2,2,2,2,2,1,2,1,2,1,2,1,2,1,1,1,2,1],
         [1,2,1,2,2,2,1,2,2,2,1,2,2,2,2,2,1,2,2,2,2,2,1,2,2,2,1,2,2,2,1,2,1],
         [1,2,1,2,1,2,1,2,1,2,2,2,1,2,1,1,1,1,1,2,1,2,2,2,1,2,1,2,1,2,1,2,1],
         [1,2,2,2,1,2,2,2,1,2,2,2,1,2,2,2,2,2,2,2,1,2,2,2,1,2,2,2,1,2,2,2,1],
@@ -21,11 +21,27 @@ var xPos = 800;
 var yPos = 350;
 var score = 0;
 
-var ghost = {
-    xPos: 750,
-    yPos: 300,
-    active: false
+var ghosts = {
+    ghost1 : {
+        xPos: 750,
+        yPos: 300
+        },
+    ghost2 : {
+        xPos: 850,
+        yPos: 300
+        },
+    ghost3 : {
+        xPos: 750,
+        yPos: 400
+        },
+    ghost4 : {
+        xPos: 850,
+        yPos: 400
+        },
 }
+
+var moves = 0;
+var gameOver = false;
 
 function showWorld(){
     var htmlString="";
@@ -38,76 +54,136 @@ function showWorld(){
                 htmlString += '<div class="coin"></div>';
             } else if(world[y][x] === 0){
                 htmlString += '<div class="empty"></div>';
+            } else if(world[y][x] === 3){
+                htmlString += '<div class="fruit"></div>'
             }
         }
         htmlString += '</div>';
     }
-    // console.log(htmlString);
     document.getElementById('world').innerHTML = htmlString;
 }
 
 document.onkeydown = function(event){
-    console.log(event);
+    // console.log(event);
+    if(!gameOver){
+        //left
+        if(event.keyCode == 37 && world[yPos/50][(xPos/50)-1] !=1){
+            xPos -= 50;
+            moves++;
+            rollForFruit();
+            moveGhosts();
+        } 
 
-    //left
-    if(event.keyCode == 37 && world[yPos/50][(xPos/50)-1] !=1){
-        xPos -= 50;
-    } 
+        //up
+        else if( event.keyCode ==38 && world[(yPos/50)-1][xPos/50] !=1){
+            yPos -= 50;
+            moves++;
+            rollForFruit();
+            moveGhosts();
+        }  
 
-    //up
-    else if( event.keyCode ==38 && world[(yPos/50)-1][xPos/50] !=1){
-        yPos -= 50;
-    }  
+        //right
+        else if( event.keyCode ==39 && world[yPos/50][(xPos/50)+1] !=1){
+            xPos += 50;
+            moves++;
+            rollForFruit();
+            moveGhosts();
+        } 
 
-    //right
-    else if( event.keyCode ==39 && world[yPos/50][(xPos/50)+1] !=1){
-        xPos += 50;
-    } 
+        //down
+        else if( event.keyCode ==40 && world[(yPos/50)+1][xPos/50] !=1){
+            yPos += 50;
+            moves++;
+            rollForFruit();
+            moveGhosts();
+        }
 
-    //down
-    else if( event.keyCode ==40 && world[(yPos/50)+1][xPos/50] !=1){
-        yPos += 50;
+        document.getElementById('pacman').style.top = yPos+'px';
+        document.getElementById('pacman').style.left = xPos+'px';
+        checkColletibles();
+        checkForGhosts();
     }
-    //left 37
-    //up 38
-    //right 39
-    //up 40
-
-    document.getElementById('pacman').style.top = yPos+'px';
-    document.getElementById('pacman').style.left = xPos+'px';
-    checkCollision();
 };
 
-function checkCollision(){
+function checkColletibles(){
     var currentCoords = [xPos/50, yPos/50];
     if (world[yPos/50][xPos/50] === 2){
         world[yPos/50][xPos/50] = 0;
         score ++;
         showWorld();
         showScore();
-    } else if(world[yPos/50][xPos/50] === 1){
-        console.log("shit")
-        document.getElementsByClassName('brick').style.backgroundColor= "red";
-        document.getElementByClassName('brick').style.backgroundColor = "white";
-        document.getElementByClassName('brick').style.backgroundColor = "red";
-        document.getElementByClassName('brick').style.backgroundColor = "white";
-        document.getElementByClassName('brick').style.backgroundColor = "red";
-        document.getElementByClassName('brick').style.backgroundColor = "white";
+    } else if(world[yPos/50][xPos/50] === 3){
+        world[yPos/50][xPos/50] = 0;
+        score += 50;
+        showWorld();
+        showScore();
     }
-    
 }
 
 function showScore(){
     document.getElementById('score').innerText = "Score: "+ score;
 }
 
+function rollForFruit(){
+    if(moves > 10 && moves%5 === 0){
+        var randY = Math.floor(Math.random()*(world.length-1));
+        var randX = Math.floor(Math.random()*(world[randY].length-1));
+        // console.log(randY);
+        // console.log(randX);
+        if(world[randY][randX] === 0 ){
+            world[randY][randX] = 3;
+        }
+    }
+}
+
+function moveGhosts(){
+    if(moves>10){
+        for(var gh in ghosts){
+            var direction = Math.floor(Math.random()*4)+1;
+
+            //this could be combined with player movement with some effort
+            //left
+            if(direction === 1 && world[ghosts[gh].yPos/50][(ghosts[gh].xPos/50)-1] !==1){
+                ghosts[gh].xPos -= 50;
+            } 
+
+            //up
+            else if(direction === 2 && world[(ghosts[gh].yPos/50)-1][ghosts[gh].xPos/50] !==1){
+                ghosts[gh].yPos -= 50;
+            }  
+
+            //right
+            else if(direction === 3 && world[ghosts[gh].yPos/50][(ghosts[gh].xPos/50)+1] !==1){
+                ghosts[gh].xPos += 50;
+            } 
+
+            //down
+            else if(direction === 4 && world[(ghosts[gh].yPos/50)+1][ghosts[gh].xPos/50] !==1){
+                ghosts[gh].yPos += 50;
+            }
+
+            document.getElementById(gh).style.top = ghosts[gh].yPos+'px';
+            document.getElementById(gh).style.left = ghosts[gh].xPos+'px';
+        }
+    }
+}
+
+function checkForGhosts(){
+    for(var gh in ghosts){
+        if(ghosts[gh].xPos === xPos && ghosts[gh].yPos ===yPos){
+            gameOver = true;
+            alert('Game Over');
+        }
+    }
+}
+
 showWorld();
 
 
 
-//build a world
-//put pacman on board
-//move pacman
-//eat coins
+//****build a world
+//*****put pacman on board
+//*****move pacman
+//****eat coins
 //add fruit
 //randomly generate board
